2cb9d91b73570cfc8377fca92b51102bd5b36207
TMonton <tomasmonton261@gmail.com>
2026-01-28 00:08:20 -0300

    feat: Se crearon los componentes de autenticación y correcciones

---
+++ Frontend/LionGoldenFitness/src/app/home/componentes-de-autenticacion/registro-nuevo-usuario/registro-nuevo-usuario.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-nuevo-usuario',
+  imports: [],
+  templateUrl: './registro-nuevo-usuario.html',
+  styleUrl: './registro-nuevo-usuario.css',
+})
+export class RegistroNuevoUsuario {
+
+}
