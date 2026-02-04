95d22744e162a7423de06b2baf79295a0b891b7a
Franco <francoavila9356@gmail.com>
2026-02-04 20:22:45 -0300

    feat:correcion de codigo

--- Frontend/LionGoldenFitness/src/app/home/componentes-de-autenticacion/iniciar-sesion/iniciar-sesion.ts
+++ Frontend/LionGoldenFitness/src/app/home/componentes-de-autenticacion/iniciar-sesion/iniciar-sesion.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-iniciar-sesion',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './iniciar-sesion.html',
   styleUrl: './iniciar-sesion.css',
 })
 export class IniciarSesion {
+  form!: FormGroup;
 
-}
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      user: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  } 
+
+ 
+  onSubmit() {
+    if (this.form.valid) {
+      console.log("Datos del formulario:", this.form.value);
+    }
+  }
+
+} 
