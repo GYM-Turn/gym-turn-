f94fc1c71f0ca922a08620121d2bef5128e6d9bc
TMonton <tomasmonton261@gmail.com>
2026-02-08 02:11:02 -0300

    fix: Se corrigieron errores al cargar la página, instalando la libreria correspondiente

--- Frontend/LionGoldenFitness/src/app/app.ts
+++ Frontend/LionGoldenFitness/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as AOS from 'aos'; // Importamos la librería AOS
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('LionGoldenFitness');
+
+  ngOnInit() {
+    // Inicializamos las animaciones
+    AOS.init({
+      duration: 800,    // Duración de la animación (800ms es ideal para que sea ágil)
+      easing: 'ease-in-out', // Suavizado de la transición
+      once: false,      // Permite que se repita la animación si subes y bajas
+      mirror: true,     // Anima elementos al scrollear hacia arriba también
+      offset: 120       // Empieza la animación 120px antes de que el elemento aparezca
+    });
+  }
 }
